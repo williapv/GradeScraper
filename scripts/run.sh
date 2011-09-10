8a7b02bf076dd5b71ab94f475db49c211b2e9496
Paul Williams <williapv@gmail.com>
2011-09-09 20:46:28 -0500

    made script work from any directory so it will work with crontab

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-export JAR_CLASSPATH=../target/gradescraper-1.0-SNAPSHOT.jar
-export JAR_CLASSPATH=$JAR_CLASSPATH:../target/lib/activation-1.1.jar
-export JAR_CLASSPATH=$JAR_CLASSPATH:../target/lib/commons-codec-1.2.jar
-export JAR_CLASSPATH=$JAR_CLASSPATH:../target/lib/commons-httpclient-3.0.1.jar
-export JAR_CLASSPATH=$JAR_CLASSPATH:../target/lib/commons-logging-1.0.3.jar
-export JAR_CLASSPATH=$JAR_CLASSPATH:../target/lib/mail-1.4.4.jar
+current_dir=$(pwd)
+script_dir=$(dirname $0)
+
+echo $current_dir
+echo $script_dir
+# Absolute path this script is in, thus /home/user/bin
+BASEDIR=$current_dir/$script_dir
+export JAR_CLASSPATH=$BASEDIR/../target/gradescraper-1.0-SNAPSHOT.jar
+export JAR_CLASSPATH=$JAR_CLASSPATH:$BASEDIR/../target/lib/activation-1.1.jar
+export JAR_CLASSPATH=$JAR_CLASSPATH:$BASEDIR/../target/lib/commons-codec-1.2.jar
+export JAR_CLASSPATH=$JAR_CLASSPATH:$BASEDIR/../target/lib/commons-httpclient-3.0.1.jar
+export JAR_CLASSPATH=$JAR_CLASSPATH:$BASEDIR/../target/lib/commons-logging-1.0.3.jar
+export JAR_CLASSPATH=$JAR_CLASSPATH:$BASEDIR/../target/lib/mail-1.4.4.jar
 echo $JAR_CLASSPATH
-java -cp $JAR_CLASSPATH com.williapv.grades.gradesscraper.ScrapeGrades
+java -cp $JAR_CLASSPATH com.williapv.grades.gradescraper.ScrapeGrades
